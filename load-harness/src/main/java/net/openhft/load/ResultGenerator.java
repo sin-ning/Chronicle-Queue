fba73988eff23f37dfb4c836f7290a19dd7dcc86
epickrram <mark@epickrram.com>
2017-12-20 11:41:24 +0000

    set rollcycle

--- load-harness/src/main/java/net/openhft/load/ResultGenerator.java
+++ load-harness/src/main/java/net/openhft/load/ResultGenerator.java
@@ -3,6 +3,7 @@ package net.openhft.load;
 import net.openhft.chronicle.bytes.MethodReader;
 import net.openhft.chronicle.core.Jvm;
 import net.openhft.chronicle.core.util.Histogram;
+import net.openhft.chronicle.queue.RollCycles;
 import net.openhft.chronicle.queue.impl.single.SingleChronicleQueue;
 import net.openhft.chronicle.queue.impl.single.SingleChronicleQueueBuilder;
 import net.openhft.chronicle.wire.Marshallable;
@@ -36,7 +37,7 @@ public final class ResultGenerator {
             System.out.println(m);
         }, (c, m, t) -> {System.out.println(m); if (t != null) {t.printStackTrace();}}, (c, m, t) -> System.out.println(m));
         try (final SingleChronicleQueue queue =
-                     SingleChronicleQueueBuilder.binary(lastStageConfig.getOutputPath()).build();
+                     SingleChronicleQueueBuilder.binary(lastStageConfig.getOutputPath()).rollCycle(RollCycles.HOURLY).build();
              final Writer resultsWriter = new FileWriter("results.txt", false)) {
             final MethodReader methodReader = queue.createTailer().methodReader(new CapturingReceiver(resultsWriter));
             while (methodReader.readOne()) {
